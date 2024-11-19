0663ceaafa23df37186bcbef54829e5899bbc710
stefan-laux <stefan.laux@nodepulse.ch>
2024-11-19 11:10:14 +0100

    init

---
+++ project/ticketsystem/src/main/java/ch/tbz/ticketsystem/TicketsystemApplication.java
@@ -0,0 +1,13 @@
+package ch.tbz.ticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsystemApplication.class, args);
+	}
+
+}

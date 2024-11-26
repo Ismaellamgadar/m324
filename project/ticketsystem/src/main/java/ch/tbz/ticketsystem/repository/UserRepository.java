6c1209b22d88d50b56e48bab82528e59782520d2
Ismael Lamgadar <ilamgadar@gmail.com>
2024-11-26 10:12:26 +0100

    feature/16 add user endpoint (#20)
    * 16 add User Entity and get-Endpoint

---
+++ project/ticketsystem/src/main/java/ch/tbz/ticketsystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ch.tbz.ticketsystem.repository;
+
+import ch.tbz.ticketsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> getUserByUsername(String username);
+}
